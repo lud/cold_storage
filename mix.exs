d1d6389a112ccffdb9aeffd82293e9a1e24056a2
lud <ludovic@demblans.com>
2025-10-15 13:07:29 +0200

    chore(deps): Added dev dependencies

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ColdStorage.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -19,7 +20,29 @@ defmodule ColdStorage.MixProject do
 
   defp deps do
     [
-      {:briefly, "~> 0.5.1", only: [:test]}
+      # Dev / Test
+      {:briefly, "~> 0.5.1", only: [:test]},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.38", only: [:dev, :test, :doc], runtime: false}
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [
+        dialyzer: :test
+      ]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      flags: [:unmatched_returns, :error_handling, :unknown, :extra_return],
+      list_unused_filters: true,
+      plt_add_deps: :app_tree,
+      plt_add_apps: [:ex_unit, :mix, :jsv],
+      plt_local_path: "_build/plts"
     ]
   end
 end
